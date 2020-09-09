4398c64a968c37ab0982277e8a18f4a75f8d7544
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-09-09 15:43:28 +0200

    modifie profile photo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Jeff. I'm a software enginer and a translator (French/Perceval). You could learn to speak like a 
 Knight of the Round Table</p>
+        <p>Read <Link href="/posts/first-post"><a>this page!</a></Link></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
